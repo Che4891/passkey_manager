03d3d888ec6afb890295afbae24614f7162072ef
Che4891 <antoon.izotov@yandex.ru>
2025-12-05 07:07:36 +0200

    hot fix

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,17 @@
+// android/build.gradle.kts
+buildscript {
+    val kotlinVersion = "1.9.24"
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.0")  // Використовуйте 8.5.0 для Gradle 8.11.1
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -15,6 +29,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
